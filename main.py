f5c9c3c00e8d8c33bdfe0532a65d60a2429df53e
EricSharkey <eric@lisaneric.org>
2019-01-24 09:59:48 -0500

    Scaled menu buttons

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ class menu():
     def layout(self):
         i = 1
         for button in self.buttons:
+            button.image = pygame.transform.smoothscale(button.image,(self.rect.w,int(self.rect.h/len(self.buttons))))
+            button.rect = button.image.get_rect()
             button.rect.centerx = self.rect.centerx
             button.rect.centery = self.rect.top + (self.rect.h/(len(self.buttons) + 1))*i
             i+=1
@@ -70,7 +72,7 @@ pygame.mixer.music.load("Untitled.wav")
 
 #game function
 def game():
-    gmenu = menu(display_width * 0.75, display_height * 0.0, display_width * 1.0, display_height * 1.0)
+    gmenu = menu(display_width * 0.85, display_height * 0.0, display_width * 1.0, display_height * 1.0)
     gmenu.addbutton(startbutton)
     gmenu.addbutton(optionbutton)
     gmenu.addbutton(quitbutton)
